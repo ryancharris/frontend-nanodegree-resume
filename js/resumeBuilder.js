6ebf640741a7f13b5c95d5f665807f057e337abe
ryancharris <harrisryan1@gmail.com>
2016-04-10 15:00:03 -0400

    Finished Lesson 1 of JavaScript Basics

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,5 +1,86 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {
+	"name": "Ryan Harris",
+	"role": "Front-End Developer",
+	"contacts": {
+		"email": "harrisryan1@gmail.com",
+		"github": "ryancharrs",
+		"twitter": "@ryan_c_harris",
+		"location": "Portland, ME"
+	},
+	"welcomeMessage": "Hello!",
+	"skills": [
+		"Web Development", "Videography", "Video Editing", "Animation"
+	],
+	"bioPic": "https://pbs.twimg.com/profile_images/462414794738257920/WPdnErVt_400x400.jpeg"
+};
 
- $('#main').append("Ryan Harris");
+var education = {
+	"schools": [
+		{
+			"name": "Ithaca College",
+			"locations": "Ithaca, NY",
+			"degree": "B.S.",
+			"majors": ["Cinema Production"],
+			"dates": "2003-2007",
+			"url": "http://www.ithaca.edu"
+		}
+	],
+	"onlineCourses": [
+		{
+			"title": "Front End Developer Track",
+			"school": "Treehouse",
+			"dates": "2012 - 2016",
+			"url": "http://www.teamtreehouse.com"
+		},
+		{
+			"title": "JavaScript Basics",
+			"school": "Udacity",
+			"dates": "2016",
+			"url": "http://www.udacity.com"
+		}
+	]
+};
+
+var work = {
+	"jobs": [
+		{
+			"employer": "Freelance",
+			"title": "Web Developer",
+			"location": "Portland, ME",
+			"dates": "2016 - Present",
+			"description": "Designing & building websites",
+		},
+		{
+			"employer": "Make Sense Productions",
+			"title": "Scriptwriter",
+			"location": "Portland, ME",
+			"dates": "2015 - 2016",
+			"description": "Writing comedy for a daily satirical news show",
+		},
+		{
+			"employer": "Rutgers Film Bureau",
+			"title": "Editor / Animator / Producer",
+			"location": "New Brunwick, NJ",
+			"dates": "2014-2015",
+			"description": "Worked on 'Antarctic Edge', a documentary about climate change",
+		},
+	]
+};
+
+var projects = {
+	"projects": [
+		{
+			"title": "www.ryancharris.com",
+			"dates": "2016",
+			"description": "Redesigned my portfolio website",
+			"images": ["../images/ryancharris.png"]
+		},
+		{
+			"title": "Antarctic Edge: 70° South",
+			"dates": "2015",
+			"description": "NSF-funded documentary about climate change in Antarctica",
+			"images": ["../images/antarctic-edge.png"]
+		}
+	]
+};
+
